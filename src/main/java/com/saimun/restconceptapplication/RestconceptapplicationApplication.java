9a3fc5cbb26f2aeeb4caeea7f7e3a59eba64080c
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-04-28 23:34:58 +0600

    initialization

---
+++ src/main/java/com/saimun/restconceptapplication/RestconceptapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.saimun.restconceptapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestconceptapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestconceptapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saimun/restconceptapplication/RestconceptapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saimun.restconceptapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestconceptapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
