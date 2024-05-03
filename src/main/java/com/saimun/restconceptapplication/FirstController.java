d8c0801f890d6d97428da8e945ae7091089b0ef8
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-05-03 10:36:37 +0600

    first controller

---
+++ src/main/java/com/saimun/restconceptapplication/FirstController.java
@@ -0,0 +1,12 @@
+package com.saimun.restconceptapplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello from first controller";
+	}
+}
