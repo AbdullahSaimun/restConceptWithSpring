0596346d765801daf7a3b1ef32196eed292cc13e
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-05-03 10:41:45 +0600

    response status

--- src/main/java/com/saimun/restconceptapplication/FirstController.java
+++ src/main/java/com/saimun/restconceptapplication/FirstController.java
@@ -1,6 +1,8 @@
 package com.saimun.restconceptapplication;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,12 @@ public class FirstController {
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello from first controller";
+	}
+
+
+	@GetMapping("/hello-another")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public String sayHelloAnother() {
+		return "Another Hello from first controller";
 	}
 }
