fb8637a0c107de6e2ea9206434b401ab4e800396
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-05-07 22:06:46 +0600

    car builder class

---
+++ src/main/java/com/saimun/restconceptapplication/dto/CarBuilderTest.java
@@ -0,0 +1,57 @@
+package com.saimun.restconceptapplication.dto;
+
+public class CarBuilderTest {
+	private String make;
+	private String model;
+	private Integer year;
+
+	public CarBuilderTest(Builder builder) {
+		this.make = builder.make;
+		this.model = builder.model;
+		this.year = builder.year;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public static class Builder {
+		private String make;
+		private String model;
+		private Integer year;
+
+		public Builder make(String make) {
+			this.make = make;
+			return this;
+		}
+
+		public Builder model(String model) {
+			this.model=model;
+			return this;
+		}
+
+		public Builder year(Integer year) {
+			this.year = year;
+			return this;
+		}
+
+		public CarBuilderTest build() {
+			return new CarBuilderTest(this);
+		}
+	}
+
+	public static void main(String[] args) {
+		CarBuilderTest cb = new Builder().make("fjkdf").model("dsdf").year(32).build();
+		System.out.println(cb.make);
+		System.out.println(cb.model);
+		System.out.println(cb.year);
+	}
+}
